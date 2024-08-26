3a5c13c4e85d52fd04f526e85a6ff594dbabd4f7
Ahmadpasha88 <shaikahamad163@gmail.com>
2024-08-26 15:15:26 +0530

    trying to deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const sequelize = require("./db");
 const cors = require("cors");
 
+// Import models
 const User = require("./models/User");
 const Blog = require("./models/Blog");
 const Comment = require("./models/Comment");
@@ -13,26 +14,24 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
+// Authenticate and sync database
 sequelize
   .authenticate()
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.error("Unable to connect to the database:", err));
 
-// sequelize
-//   .sync({ force: false })
-//   .then(() => {
-//     console.log("Database & tables created!");
-   
-    
-//   })
-//   .catch((err) => {
-//     console.error("Error synchronizing the database:", err);
-//   });
+// Ensure models are associated properly if they are interdependent
+User.hasMany(Blog, { foreignKey: 'user_id' });
+Blog.belongsTo(User, { foreignKey: 'user_id' });
 
+Blog.hasMany(Comment, { foreignKey: 'blog_id' });
+Comment.belongsTo(Blog, { foreignKey: 'blog_id' });
 
+User.hasMany(Comment, { foreignKey: 'user_id' });
+Comment.belongsTo(User, { foreignKey: 'user_id' });
 
 // Synchronize models with the database
-sequelize.sync({ alter: true })
+sequelize.sync({ alter: true, logging: console.log })
     .then(() => {
         console.log("All models were synchronized successfully.");
     })
@@ -40,11 +39,8 @@ sequelize.sync({ alter: true })
         console.error("Error synchronizing the database:", error);
     });
 
-  app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server running on port ${process.env.PORT || 5000}`);
-  });
-
-  app.get('/api/check-tables', async (req, res) => {
+// API route to check tables in production
+app.get('/api/check-tables', async (req, res) => {
     try {
         const tables = await sequelize.getQueryInterface().showAllTables();
         res.status(200).json(tables);
@@ -53,18 +49,27 @@ sequelize.sync({ alter: true })
         res.status(500).json({ message: "Failed to fetch tables" });
     }
 });
-  
+
+// Import routes
 const userRoutes = require("./routes/UserRoutes");
 const blogRoutes = require("./routes/BlogPostsRoutes");
 const commentRoutes = require("./routes/CommentRoutes");
 
+// Use routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", blogRoutes);
 app.use("/api/comments", commentRoutes);
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = { User, Blog, Comment };
